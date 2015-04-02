bd660a0105f447d4f62c54293c42c27d3273654a
Julio Canares <juliocanares@gmail.com>
2015-04-02 17:39:08 -0500

    change user routes

--- app/routes/user.js
+++ app/routes/user.js
@@ -5,6 +5,20 @@ var config = require('../../config/config');
 var controller = require(config.controllersDir + "/user");
 var middlewares = require(config.middlewaresDir + '/app');
 
-router.post('/specific', controller.specific);
+router.get('/:username', controller.profile);
+router.post('/:username/update', controller.update);
+router.get('/:username/configuration', controller.configuration);
+
+router.get('/:username/portfolio/upload', controller.upload);
+router.post('/:username/portfolio/upload', controller.postUpload);
+
+//router.get('/profile', controller.profile);
+
+//router.get('/store', controller.configuration);
+
+router.post('/follow/:idUser', controller.follow);
+router.post('/unfollow/:idUser', controller.unfollow);
+router.post('/like/:idWork', controller.like);
+router.post('/unlike/:idWork', controller.unlike);
 
 module.exports = router;
