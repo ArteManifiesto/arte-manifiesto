6e9bda9bd0522114fba9c02a2951943787d40947
Julio Canares <juliocanares@gmail.com>
2015-04-02 17:37:12 -0500

    add tag model

---
+++ app/models/tag.js
@@ -0,0 +1,14 @@
+module.exports = function (sequelize, DataTypes) {
+    var Tag = sequelize.define('Tag', {
+            name: DataTypes.STRING
+        }, {
+            classMethods: {
+                associate: function (models) {
+                    //TODO maybe tag need delete when the work is deleted?
+                    Tag.belongsToMany(models.Work);
+                }
+            }
+        }
+    );
+    return Tag;
+};
