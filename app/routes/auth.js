0dc417af48608f564906dd57d8abc995f96631a8
Julio Canares <juliocanares@gmail.com>
2015-04-02 17:37:59 -0500

    add verify , forgot and reset routes

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -11,12 +11,26 @@ var authenticateParams = {
     failureRedirect: '/'
 };
 var fbPermissions = [
-    'email', 'user_about_me'
+    'email',
+    'user_about_me',
+    'user_birthday',
+    'user_friends',
+    'user_interests'
 ];
 
 router.post('/login', controller.login);
 router.post('/signup', controller.signup);
 router.get('/logout', controller.logout);
+
+router.get('/verify/:token', controller.verify);
+router.get('/success', controller.verifySuccess);
+router.get('/failure', controller.verifyFailure);
+
+router.get('/forgot', controller.forgot);
+router.post('/forgot', controller.forgotCreate);
+
+router.get('/reset/:token', controller.reset);
+router.post('/reset/:token', controller.resetVerify);
 
 router.get('/facebook', passport.authenticate('facebook', {scope: fbPermissions}));
 router.get('/facebook/callback', passport.authenticate('facebook', authenticateParams));
