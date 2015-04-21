175debe96520e2763e73ecdf1b7f429923e02134
Julio Canares <juliocanares@gmail.com>
2015-04-21 01:46:40 -0500

    add producttype model

---
+++ app/models/producttype.js
@@ -0,0 +1,20 @@
+module.exports = function (sequelize, DataTypes) {
+    var ProductType = sequelize.define('ProductType', {
+            name: {
+                type: DataTypes.STRING,
+                set: function (value) {
+                    this.setDataValue('nameSlugify', global.slugify(value));
+                    this.setDataValue('name', value);
+                }
+            },
+            nameSlugify: DataTypes.STRING
+        }, {
+            classMethods: {
+                associate: function (models) {
+                    ProductType.hasMany(models.Product);
+                }
+            }
+        }
+    );
+    return ProductType;
+};
