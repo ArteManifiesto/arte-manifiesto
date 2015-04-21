6c5adad569ec1113d4ad3b530eb23dbe8434fae6
Julio Canares <juliocanares@gmail.com>
2015-04-21 01:46:19 -0500

    add product model

--- app/models/product.js
+++ app/models/product.js
@@ -1,10 +1,37 @@
+var _ = require('lodash');
+
+var Chance = require('chance');
+var chance = new Chance();
+
 module.exports = function (sequelize, DataTypes) {
     var Product = sequelize.define('Product', {
-            name: DataTypes.STRING
+            name: {
+                type: DataTypes.STRING,
+                set: function (value) {
+                    this.setDataValue('nameSlugify', global.slugify(value));
+                    this.setDataValue('name', value);
+                }
+            },
+            nameSlugify: DataTypes.STRING,
+            price: DataTypes.INTEGER
         }, {
             classMethods: {
                 associate: function (models) {
+                    Product.belongsToMany(models.User, {as: 'ProductViewers', through: 'ProductViewers'});
                     Product.belongsTo(models.Work, {onDelete: 'cascade'});
+                    Product.belongsTo(models.ProductType);
+
+                    Product.hasMany(models.ProductFeatured);
+
+
+
+                }
+            },
+            hooks: {
+                afterCreate: function (product, options) {
+                    return global.db.ProductType.find(_.random(1, 5)).then(function (type) {
+                        return type.addProduct(product);
+                    });
                 }
             }
         }
