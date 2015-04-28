979c7d13a5b6ba451b3342db1fc080d3243b919a
Julio Canares <juliocanares@gmail.com>
2015-04-28 12:41:29 -0500

    add product route

---
+++ app/routes/user-product.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+router.mergeParams = true;
+
+var config = require('../../config/config');
+var controller = require(config.controllersDir + "/user-work");
+
+router.get('/add', controller.add);
+router.get('/:nameWork', controller.work);
+
+router.post('/create', controller.create);
+router.post('/delete', controller.workDelete);
+router.post('/update', controller.workUpdate);
+
+//TODO add middleware for check if work exists
+router.post('/featured', controller.featured);
+router.post('/unfeatured', controller.unFeatured);
+router.post('/like', controller.like);
+router.post('/unlike', controller.unLike);
+
+router.post('/public' , controller.public);
+router.post('/private' , controller.private);
+
+/*
+ router.post('/work/add/collection', controller.workAddCollection);
+ router.post('/work/switch/collection', controller.workSwitchCollection);
+ */
+
+module.exports = router;
