71395b269052f2ea33e431f704e89fc63cd9ae24
Hanshavin <hansevangelista@gmail.com>
2015-05-25 23:33:14 -0500

    get products

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,9 +1,9 @@
 function Profile (el, data) {
 
 	var templates =[_.template( $( "#work-template" ).html() )]
-	var pagination = []
+	var paginations = []
 
-	var paths = ['portfolio']
+	var paths = ['portfolio', 'products']
 
 	var buttons = [data.menus[0], data.menus[1], data.menus[2], data.menus[3], data.menus[4]],
 			containers = [data.elements[0], data.elements[1], data.elements[2], data.elements[3], data.elements[4]],
@@ -38,17 +38,20 @@ function Profile (el, data) {
 
 		$.post( url, function( response ) {
 			console.log('response: ', response)
-			pagination[index] = response.pagination
-			renderElements(index, response.works)
+			paginations[index] = response.pagination
+			console.log('index: ', index)
+			if(index == 0) renderElements(index, response.works)
+			if(index == 1) renderElements(index, response.products)
 			actives[index] = true
 		});
 
 	}
 
 	function renderElements (index, elements) {
+		console.log('renderElements', index, elements)
 		if(elements.length != 0){
 			addElements(index, elements)
-			if(pagination[index].page < pagination[index].pages){
+			if(paginations[index].page < paginations[index].pages){
 				// containers[index].querySelector('more').classList.remove('hidden')
 			}
 				// moreSection.classList.remove('hidden')
@@ -57,7 +60,33 @@ function Profile (el, data) {
 		// emptyResult[].classList.add('visible')
 		// containers[index].querySelector('.empty').style.add('visible')
 	}
-	
+
+	function addElements (index, elements) {
+		for (var i = 0; i < elements.length; i++) {
+			var object = makeObject(templates[index], elements[i])
+			console.log('object: ', object)
+			// createObject(object, elements[i])
+
+			if(index != 0){
+				// containers.appendChild(object)
+				console.log('container: ', containers[index].children[0])
+				containers[index].children[0].appendChild(object)
+			}
+			else{
+				console.log('container: ', containers[index].children[0])
+				salvattore['append_elements'](containers[index].children[0], [object])
+				// salvattore['append_elements'](containers[index], [object])
+			}
+		}
+	}
+
+	function makeObject (template, data) {
+		var objectString = template(data)
+		var div = document.createElement('div')
+		div.innerHTML = objectString
+		return div.children[0]
+	}
+
 	setup()
 }
 
