d202a0b1ee8f7853104039c9cd7ac864776ef362
Hanshavin <hansevangelista@gmail.com>
2015-07-07 11:48:54 -0500

    right resign interests

---
+++ public/js/complite-profile.js
@@ -0,0 +1,11 @@
+function CompileProfile () {
+
+	var saveButton = document.querySelector('.button-solid'),
+			username = document.querySelector('input[name="username"]');
+
+	console.log('saveButton', saveButton)
+	console.log('username', username)
+
+}
+
+var compileProfile = new CompileProfile()
