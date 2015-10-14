c2a49703406260a638e69f9aff73ad0248306d6f
Julio Canares <juliocanares@gmail.com>
2015-10-13 20:23:33 -0700

    add carrouselitem

---
+++ app/javascript/elements/CarrouselItem.js
@@ -0,0 +1,17 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+var APP = APP || {};
+
+APP.CarrouselItem = function (data) {
+    APP.BaseElement.call(this, data, 'carrouselItem');
+};
+
+APP.CarrouselItem.prototype = Object.create(APP.BaseElement.prototype);
+
+APP.CarrouselItem.constructor = APP.CarrouselItem;
+
+APP.CarrouselItem.prototype.listeners = function () {
+  this.view.click(this.gotoSingle.bind(this));
+};
