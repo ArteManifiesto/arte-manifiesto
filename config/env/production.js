4570c9703551952ef982c393b3973d2403764b5e
Julio Canares <juliocanares@gmail.com>
2015-10-30 00:38:13 -0700

    new version 2

--- config/env/production.js
+++ config/env/production.js
@@ -1,25 +1,24 @@
 module.exports = {
     db: {
-        name: "d1md7ij022218u",
-        username: "fqvcfvthlpwrfy",
-        password: "45m8-2F68ziIApvChPKIbXHmii",
-        dialect: "postgres",
-        protocol: "postgres",
+        name: "heroku_082ce63f99d3d82",
+        username: "be72f71bb5432f",
+        password: "520f5767",
+        dialect: "mysql",
+        protocol: "mysql",
         port: "5432",
-        host: "ec2-107-21-226-77.compute-1.amazonaws.com",
+        host: "us-cdbr-iron-east-02.cleardb.net",
         logging: true
     },
     app: {
-        name: "Zanna"
+        name: "Arte"
     },
     facebook: {
-        clientID: "921435404563523",
-        clientSecret: "e89c8736b63b932180796911b24ff617",
-        callbackURL: "http://zanna.co/auth/facebook/callback"
+        clientID: "1473636902857068",
+        clientSecret: "defdc485a2b8b1ba309c735d8e975c21",
+        callbackURL: "http://artemanifiesto.com/auth/facebook/callback/"
     },
-    twitter: {
-        clientID: "7hysPpVW2L4lNpsinsn0lcrWi",
-        clientSecret: "P7gFnno6ej0NVohVchClIw0ee7Bb9z0c7UV9ddIXQ9uM1vG5w9",
-        callbackURL: "http://zanna.co/auth/twitter/callback"
+    recaptcha: {
+        publicKey: '6LdnUQoTAAAAALPzF3glOaqzGcbhBrmT1yQwVYls',
+        privateKey: '6LdnUQoTAAAAACVrGpYvAvGv3ylnBsnkIHXdPLX9'
     }
 };
