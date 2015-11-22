5725aa16a606ec6528d895540f51ae174a7dfec9
Julio Canares <juliocanares@gmail.com>
2015-11-22 11:43:58 -0500

    change environment

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ var _ = require('lodash');
 
 module.exports = _.assign(
     require(__dirname + '/../config/env/all.js'),
-    require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.js') || {}
+    require(__dirname + '/../config/env/env.js') || {}
 );
