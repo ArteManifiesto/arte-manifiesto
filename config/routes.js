5c6b4c8531aef9239ca0011669a953bdb5d00c0a
Julio Canares <juliocanares@gmail.com>
2015-12-04 04:32:30 -0500

    delete blog  models

--- config/routes.js
+++ config/routes.js
@@ -8,7 +8,6 @@ var express = require('express');
 var pagesRouter = require(global.cf.routes + '/pages');
 var authRouter = require(global.cf.routes + '/auth');
 var adminRouter = require(global.cf.routes + '/admin');
-var blogRouter = require(global.cf.routes + '/blog');
 var userRouter = require(global.cf.routes + '/user');
 
 /**
@@ -19,6 +18,5 @@ exports.init = function (app) {
     app.use('/', pagesRouter);
     app.use('/auth', authRouter);
     app.use('/admin', adminRouter);
-    app.use('/blog', blogRouter);
     app.use('/user/:username', userRouter);
 };
