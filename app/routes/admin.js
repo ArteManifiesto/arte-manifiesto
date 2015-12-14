727198d75f4b655d6246980fd0e78428e9225e72
Julio Canares <juliocanares@gmail.com>
2015-12-13 23:04:13 -0500

    remove admin controller & route

--- app/routes/admin.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controller = require(global.cf.controllers + "/admin");
-
-router.get('/' , controller.index);
-
-module.exports = router;
