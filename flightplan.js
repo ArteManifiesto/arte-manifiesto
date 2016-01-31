d39e1ca482e2b011b0e01d3a6158ae2bce706a0d
Julio Canares <juliocanares@gmail.com>
2016-01-31 02:43:01 -0500

    add config production in flightplan

--- flightplan.js
+++ flightplan.js
@@ -32,6 +32,7 @@ plan.local(function (local) {
   local.log('Copy files to remote hosts');
   var filesToCopy = local.exec('git ls-files', {silent: true});
   filesToCopy.stdout += "production\n";
+  filesToCopy.stdout += "config/config-production\n";
   local.log(JSON.stringify(filesToCopy));
   local.log(tmpDir);
   local.transfer(filesToCopy, '/tmp/' + tmpDir);
@@ -41,12 +42,18 @@ plan.remote(function (remote) {
   remote.log('Move folder to root');
   remote.sudo('cp -R /tmp/' + tmpDir + ' ~', {user: username});
   remote.rm('-rf /tmp/' + tmpDir);
-  //
 
   remote.mv(tmpDir + '/production -f .env', {user: username});
+
+  remote.rm('-rf ' + tmpDir + '/config/config.json', {user: username});
+  remote.mv(tmpDir + '/config/config-production.json -f config.json', {user: username});
+
   //
   remote.log('Install dependencies');
   remote.sudo('npm --production --prefix ~/' + tmpDir + ' install ~/' + tmpDir, {user: username});
+
+  remote.exec('cd ~/' + tmpDir);
+  remote.sudo('sequelize db:migrate', {user: username});
 
   remote.log('Reload application');
   remote.sudo('ln -snf ~/' + tmpDir + ' ~/' + appName, {user: username});
