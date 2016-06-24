bfdc42e514066f24d191dcde0f84d295fc8c3a61
RenaruX <renarux.92@gmail.com>
2016-06-24 17:59:28 -0500

    cagandola prueba 3

--- app/migrations/20160623190044-add_chapters_table.js
+++ app/migrations/20160623190044-add_chapters_table.js
@@ -15,9 +15,33 @@ module.exports = {
         updatedAt: {
           type: Sequelize.DATE
         },
-        video: Sequelize.STRING
+        video: Sequelize.STRING,
+        titulo: Sequelize.STRING,
+        descripcion: Sequelize.STRING
       }
-    )
+    );
+    queryInterface.bulkInsert('Chapters', [
+      { video: "https://www.youtube.com/embed/-4j7WQ1Sogg", 
+      titulo: "capitulo1", 
+      descripcion: "esta es una descripcion de prueba", 
+      createdAt: Date.now(), 
+      updatedAt: Date.now() },
+      { video: "https://www.youtube.com/embed/-4j7WQ1Sogg", 
+      titulo: "capitulo2", 
+      descripcion: "esta es una descripcion de prueba", 
+      createdAt: Date.now(), 
+      updatedAt: Date.now() },
+      { video: "https://www.youtube.com/embed/-4j7WQ1Sogg", 
+      titulo: "capitulo3", 
+      descripcion: "esta es una descripcion de prueba", 
+      createdAt: Date.now(), 
+      updatedAt: Date.now() },
+      { video: "https://www.youtube.com/embed/-4j7WQ1Sogg", 
+      titulo: "capitulo4", 
+      descripcion: "esta es una descripcion de prueba", 
+      createdAt: Date.now(), 
+      updatedAt: Date.now() }
+    ]);
   },
 
   down: function(queryInterface, Sequelize) {
--- app/models/chapter.js
+++ app/models/chapter.js
@@ -1,6 +1,8 @@
 module.exports = function(sequelize, DataTypes) {
   var Chapter = sequelize.define('Chapter', {
-  	video: DataTypes.STRING
+  	video: DataTypes.STRING,
+  	titulo: DataTypes.STRING,
+  	descripcion: DataTypes.STRING
   });
   return Chapter;
 };
