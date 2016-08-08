7c8ff08fbc5bbd1a2075088fa958cc6b61458ad7
juliocanares <juliocanares@gmail.com>
2016-08-08 16:22:13 -0500

    orden and priority for user routes

--- app/routes/user.js
+++ app/routes/user.js
@@ -8,19 +8,22 @@ var isLoggedAdminOrOwner = [global.md.isLogged, global.md.doubleAccess];
 
 router.use(global.md.user);
 
-router.get(['/:page?', '/portfolio/:page?'], controller.profile.bind(this, 'portfolio'));
-router.get('/collections/:page?', controller.profile.bind(this, 'collections'));
-router.get('/followers/:page?', controller.profile.bind(this, 'followers'));
-router.get('/followings/:page?', controller.profile.bind(this, 'followings'));
-router.get('/products/:page?', controller.profile.bind(this, 'products'));
-
-router.get('/notifications', isLoggedAndOwner, controller.notificationsPage);
 router.post('/notifications/:page', isLoggedAndOwner, controller.notifications);
 
 router.use('/work', require(global.cf.routes + "/user-work"));
 router.use('/product', require(global.cf.routes + "/user-product"));
 router.use('/collection', require(global.cf.routes + "/user-collection"));
 router.use('/account', isLoggedAdminOrOwner, require(global.cf.routes + "/user-account"));
+
+
+
+router.get(['/:page?', '/portfolio/:page?'], controller.profile.bind(this, 'portfolio'));
+router.get('/collections/:page?', controller.profile.bind(this, 'collections'));
+router.get('/followers/:page?', controller.profile.bind(this, 'followers'));
+router.get('/followings/:page?', controller.profile.bind(this, 'followings'));
+router.get('/products/:page?', controller.profile.bind(this, 'products'));
+
+router.get('/notifications/:page?', isLoggedAndOwner, controller.notificationsPage);
 
 router.post('/isFollowing', isLoggedAndOwner, global.md.userTo, controller.isFollowing);
 router.post('/follow', isLoggedAndOwner, global.md.userTo, controller.follow);
